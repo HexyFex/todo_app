e6135200ca858695ff008deb58298c5a02418f28
Felix Seichter <felix.seichter@gmail.com>
2024-05-26 14:25:55 +0200

    - Diverse Änderungen

--- login/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-class User(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True,
-                             blank=True)  # on_delete definiert, wie sich die Datenbank verhalten soll, wenn ein referenzierter Datensatz gelöscht wird.
--- login/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views.generic.list import ListView
-from .models import Login
-
-
-# Create your views here.
-class TaskList(ListView):
-    model = Login
